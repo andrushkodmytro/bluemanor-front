54beed3f120716bdb480fc59a06e631e875107b9
dmytro <andrushkodima@gmail.com>
2021-02-25 12:05:22 +0200

    Add finish pages for multiple quiz

---
+++ student-dashboard/quiz/multiple/multiple-finish-3/index.php
@@ -0,0 +1,7 @@
+<?php 
+	$title = "Quiz multiple";
+	$header = "hasStudentDashboardMenu";
+	include '../../../../_components/header.php';
+	include 'content.html';
+	include '../../../../_components/footer.php'
+?>
