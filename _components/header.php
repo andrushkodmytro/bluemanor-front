ea75d91f56d4d6305bbdcbc48dc8cfd8e33cce78
dmytro <andrushkodima@gmail.com>
2021-02-25 09:34:30 +0200

    Add player and hide header

--- _components/header.php
+++ _components/header.php
@@ -10,7 +10,7 @@
 	}
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class='with-player'>
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
