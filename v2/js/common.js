2bfce136af3f6bf810503520225e4bdd304700b2
dmytro <andrushkodima@gmail.com>
2021-03-17 16:34:06 +0200

    Add quize report modal

--- v2/js/common.js
+++ v2/js/common.js
@@ -1,226 +1,246 @@
-$(function(){
-	//+Owl Carousel
-	if ($('.owl-carousel.owl-papers').length) {
-		$('.owl-carousel.owl-papers').owlCarousel({
-			autoplay: false,
-			autoplayHoverPause: true,
-			items: 1,
-			animateOut: 'fadeOut',
-			nav: false,
-			navText: [,],
-			loop: true
-		});
-	}
-	if ($('.owl-carousel.owl-letters').length) {
-		$('.owl-carousel.owl-letters').owlCarousel({
-			autoplay: false,
-			autoplayHoverPause: true,
-			items: 1,
-			animateOut: 'fadeOut',
-			dots: false,
-			nav: true,
-			navText: [,],
-			loop: true
-		});
-	}
-	if ($('.owl-carousel.owl-studentCourse').length) {
-		$('.owl-carousel.owl-studentCourse').owlCarousel({
-			autoplay: false,
-			autoplayHoverPause: true,
-			// autoWidth: true,
-			animateOut: 'fadeOut',
-			dots: false,
-			nav: true,
-			navText: [,],
-			loop: false,
-			items: 6,
-			slideBy: 6,
-			responsiveClass: true,
-			responsive: {
-				0: {
-					autoplay: false,
-					autoplayHoverPause: true,
-					loop: false,
-					slideBy: 1,
-					// items: 2.5
-					items: 2
-				},
-				480: {
-					autoplay: false,
-					autoplayHoverPause: true,
-					loop: false,
-					slideBy: 2,
-					// items: 3.5
-					items: 3
-				},
-				768: {
-					autoplay: false,
-					autoplayHoverPause: true,
-					loop: false,
-					slideBy: 3,
-					// items: 4.5
-					items: 4
-				},
-				1024: {
-					autoplay: false,
-					autoplayHoverPause: true,
-					loop: false,
-					slideBy: 4,
-					// items: 5.5
-					items: 5
-				},
-				1200: {
-					autoplay: false,
-					autoplayHoverPause: true,
-					loop: false,
-					slideBy: 5,
-					// items: 6.5
-					items: 6
-				}
-			}
-		});
-	}
-	//-Owl Carousel
+$(function() {
+  //+Owl Carousel
+  if ($('.owl-carousel.owl-papers').length) {
+    $('.owl-carousel.owl-papers').owlCarousel({
+      autoplay: false,
+      autoplayHoverPause: true,
+      items: 1,
+      animateOut: 'fadeOut',
+      nav: false,
+      navText: [ , ],
+      loop: true,
+    });
+  }
+  if ($('.owl-carousel.owl-letters').length) {
+    $('.owl-carousel.owl-letters').owlCarousel({
+      autoplay: false,
+      autoplayHoverPause: true,
+      items: 1,
+      animateOut: 'fadeOut',
+      dots: false,
+      nav: true,
+      navText: [ , ],
+      loop: true,
+    });
+  }
+  if ($('.owl-carousel.owl-studentCourse').length) {
+    $('.owl-carousel.owl-studentCourse').owlCarousel({
+      autoplay: false,
+      autoplayHoverPause: true,
+      // autoWidth: true,
+      animateOut: 'fadeOut',
+      dots: false,
+      nav: true,
+      navText: [ , ],
+      loop: false,
+      items: 6,
+      slideBy: 6,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          autoplay: false,
+          autoplayHoverPause: true,
+          loop: false,
+          slideBy: 1,
+          // items: 2.5
+          items: 2,
+        },
+        480: {
+          autoplay: false,
+          autoplayHoverPause: true,
+          loop: false,
+          slideBy: 2,
+          // items: 3.5
+          items: 3,
+        },
+        768: {
+          autoplay: false,
+          autoplayHoverPause: true,
+          loop: false,
+          slideBy: 3,
+          // items: 4.5
+          items: 4,
+        },
+        1024: {
+          autoplay: false,
+          autoplayHoverPause: true,
+          loop: false,
+          slideBy: 4,
+          // items: 5.5
+          items: 5,
+        },
+        1200: {
+          autoplay: false,
+          autoplayHoverPause: true,
+          loop: false,
+          slideBy: 5,
+          // items: 6.5
+          items: 6,
+        },
+      },
+    });
+  }
+  //-Owl Carousel
 
-	$("a[href^='#']").click(function(e) {
-		e.preventDefault();
-		var dest = $(this).attr('href');
-		// console.log(dest);
-		$('html,body').animate({ scrollTop: $(dest).offset().top }, 'slow');
-	});
-	// $("a[href^='#']").click(function(e) {
-	// 	e.preventDefault();
-	// 	const dest = $(this).attr('href');
-	// 	const target = document.querySelector(dest);
-	// 	target.scrollIntoView({behavior: 'smooth', block: 'start'})
-	// });
+  $("a[href^='#']").click(function(e) {
+    e.preventDefault();
+    var dest = $(this).attr('href');
+    // console.log(dest);
+    $('html,body').animate({ scrollTop: $(dest).offset().top }, 'slow');
+  });
+  // $("a[href^='#']").click(function(e) {
+  // 	e.preventDefault();
+  // 	const dest = $(this).attr('href');
+  // 	const target = document.querySelector(dest);
+  // 	target.scrollIntoView({behavior: 'smooth', block: 'start'})
+  // });
 
-	//+Show/hide password
-	$("button.showPassword").click(function() {
-		$(this).toggleClass("hidePassword");
-		return false;
-		// $(this).next()
-		
-		// if($(this).next().attr("type","password")) {
-		// 	$(this).next().attr("type","text");
-		// } else {
-		// 	$(this).next().attr("type","password");
+  //+Show/hide password
+  $('button.showPassword').click(function() {
+    $(this).toggleClass('hidePassword');
+    return false;
+    // $(this).next()
 
-		// }
-		// if ($(this).next().type === "password") {
-		// 	$(this).next().type = "text";
-		// } else {
-		// 	$(this).next().type = "password";
-		// }
+    // if($(this).next().attr("type","password")) {
+    // 	$(this).next().attr("type","text");
+    // } else {
+    // 	$(this).next().attr("type","password");
 
-		// if($(this).hasClass('.showPassword')){
-		// 	$(this).addClass("lalala");
-		// }
-	});
+    // }
+    // if ($(this).next().type === "password") {
+    // 	$(this).next().type = "text";
+    // } else {
+    // 	$(this).next().type = "password";
+    // }
 
-	// $("button.showPassword").change(function(){
-  
-	// 	// Check the checkbox state
-	// 	if($(this).hasClass('.hidePassword')){
-	// 		$(this).addClass("lalala")
-	// 		// Changing type attribute
-	// 		$("#password").attr("type","text");
-	// 		// Change the Text
-	// 		$("#toggleText").text("Hide");
-	// 	} else {
-	// 		// Changing type attribute
-	// 		$("#password").attr("type","password");
-	// 		// Change the Text
-	// 		$("#toggleText").text("Show");
-	// 	}
-	   
-	//    });
-	//-Show/hide password
-	
+    // if($(this).hasClass('.showPassword')){
+    // 	$(this).addClass("lalala");
+    // }
+  });
 
-$('#blackboard-btn').click(()=>{
-const $dialog = $(this).find('dialog');
-$dialog.attr( "open",function(index, attr){
-	
-	return attr ? false : true;
-} )
-})
+  // $("button.showPassword").change(function(){
 
-$('#blackboard-close-btn').click(()=>{
-	const $dialog = $(this).find('dialog');
-	$dialog.attr( "open",function(index, attr){
-		return attr ? false : true;
-	} )
-	})
+  // 	// Check the checkbox state
+  // 	if($(this).hasClass('.hidePassword')){
+  // 		$(this).addClass("lalala")
+  // 		// Changing type attribute
+  // 		$("#password").attr("type","text");
+  // 		// Change the Text
+  // 		$("#toggleText").text("Hide");
+  // 	} else {
+  // 		// Changing type attribute
+  // 		$("#password").attr("type","password");
+  // 		// Change the Text
+  // 		$("#toggleText").text("Show");
+  // 	}
 
-	$('#maximize-btn').click(()=>{
-		const dialog = $(this).parents('dialog')
-		$('dialog').toggleClass("full-black-board")
-	});
+  //    });
+  //-Show/hide password
 
-	$('#minimize-btn').click(()=>{
-		const dialog = $(this).parents('dialog')
-		$('dialog').toggleClass("full-black-board")
-	});
+  $('#blackboard-btn').click(() => {
+    const $dialog = $(this).find('dialog');
+    $dialog.attr('open', function(index, attr) {
+      return attr ? false : true;
+    });
+  });
 
+  $('#blackboard-close-btn').click(() => {
+    const $dialog = $(this).find('dialog');
+    $dialog.attr('open', function(index, attr) {
+      return attr ? false : true;
+    });
+  });
 
-	//Delete notification
-	$('.delete-btn').click(function(){
-		$(this).closest('.notification-item').addClass('restore-block')
+  $('#maximize-btn').click(() => {
+    const dialog = $(this).parents('dialog');
+    $('dialog').toggleClass('full-black-board');
+  });
 
-		$(this).closest('.note-item').addClass('restore-block')
-	})
+  $('#minimize-btn').click(() => {
+    const dialog = $(this).parents('dialog');
+    $('dialog').toggleClass('full-black-board');
+  });
 
-	//Undo delete notification
-	$('.undo-btn').click(function(){
-		$(this).closest('.notification-item').removeClass('restore-block')
-		$(this).closest('.note-item').removeClass('restore-block')
-	})
+  //Delete notification
+  $('.delete-btn').click(function() {
+    $(this).closest('.notification-item').addClass('restore-block');
 
-	//View more
-	$('#view-more').click(function(e){
-		e.preventDefault();
-		
-		$('.notification-item').clone().appendTo($('.notification-container'))
-		$('.note-item').clone().appendTo($('.note-container'))
-		
-		$(this).css('display', 'none')
-	})
+    $(this).closest('.note-item').addClass('restore-block');
+  });
 
+  //Undo delete notification
+  $('.undo-btn').click(function() {
+    $(this).closest('.notification-item').removeClass('restore-block');
+    $(this).closest('.note-item').removeClass('restore-block');
+  });
 
-	  // Info modal
-		const $infoModal =  $('#add-note-modal');
+  //View more
+  $('#view-more').click(function(e) {
+    e.preventDefault();
 
-		$('.edit-btn').click(()=>{
-			$infoModal.css('display', 'flex').find('dialog').attr( "open", true)
-		});
-			
-	
-		$infoModal.find('.closeButton').click(()=>{
-			$infoModal.css('display', 'none').find('dialog').attr( "open", false)
-		})
-	
-	// 	ClassicEditor
-	// 	.create( document.querySelector( '#editor' ), {
-	// 		// toolbar: [ 'bold', 'italic', 'underline' ],
-	// 		// heading: {
-	// 		// 		options: [
-	// 		// 				{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-	// 		// 				{ model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-	// 		// 				{ model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
-	// 		// 		]
-	// 		// }
-	// }  )
-	// 	.catch( error => {
-	// 			console.error( error );
-	// 	} );
+    $('.notification-item').clone().appendTo($('.notification-container'));
+    $('.note-item').clone().appendTo($('.note-container'));
 
-	CKEDITOR.replace( 'editor'
-	// , {
-	// 	toolbar: [
-	// 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat' ] }
-	// 	]
-	// } 
-	);
-}
-);
+    $(this).css('display', 'none');
+  });
+
+  // Info modal
+  const $infoModal = $('#add-note-modal');
+
+  $('.edit-btn').click(() => {
+    $infoModal.css('display', 'flex').find('dialog').attr('open', true);
+  });
+
+  $infoModal.find('.closeButton').click(() => {
+    $infoModal.css('display', 'none').find('dialog').attr('open', false);
+  });
+
+  // 	ClassicEditor
+  // 	.create( document.querySelector( '#editor' ), {
+  // 		// toolbar: [ 'bold', 'italic', 'underline' ],
+  // 		// heading: {
+  // 		// 		options: [
+  // 		// 				{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+  // 		// 				{ model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+  // 		// 				{ model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+  // 		// 		]
+  // 		// }
+  // }  )
+  // 	.catch( error => {
+  // 			console.error( error );
+  // 	} );
+
+  // CKEDITOR.replace( 'editor'
+  // , {
+  // 	toolbar: [
+  // 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat' ] }
+  // 	]
+  // }
+  // );
+
+  const $scoreModal = $('#quiz-report-modal');
+  $scoreModal.find('.closeButton').click(() => {
+    $scoreModal.css('display', 'none').find('dialog').attr('open', false);
+  });
+
+  $scoreModal.css('display', 'flex').find('dialog').attr('open', true);
+  $('#high-scores-btn').click(() => {
+    $scoreModal.css('display', 'flex').find('dialog').attr('open', true);
+  });
+
+  $scoreModal.find('.closeButton').click(() => {
+    $scoreModal.css('display', 'none').find('dialog').attr('open', false);
+  });
+
+  // Quiz report open
+
+  $('#quiz-report-btn').click(function(e) {
+    e.preventDefault();
+
+    // Close books modal
+    $('#BookTableOfContents').attr('open', false).closest('.modal').css('display', 'none');
+
+    //  Open quiz report modal
+    $('#quiz-report-modal').css('display', 'flex').find('dialog').attr('open', true);
+  });
+});
